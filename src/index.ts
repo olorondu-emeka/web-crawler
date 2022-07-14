d3e5cd1454154219229315127b76fb35be8ac322
olorondu-emeka <oloronduchukwuemeka@gmail.com>
2022-07-14 21:53:47 +0100

    feat: store result in result.json file

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { hrtime, stdin, stdout } from 'process';
 
 import WebCrawler from './crawler';
 import { createInterface } from 'readline';
+import { writeFile } from 'fs';
 
 function example() {
   // read input from the console
@@ -13,14 +14,21 @@ function example() {
   // let url: string;
   consoleInterface.question('Enter URL (https://):\n', async (url) => {
     const crawler = new WebCrawler({ retries: 1 });
+
     const start = hrtime.bigint();
-
     const result = await crawler.crawl(url);
-
-    console.log('\nfinal result', result);
-
     const end = hrtime.bigint();
+
     console.info(`\nExecution time: ${(end - start) / BigInt(10 ** 6)}ms`);
+
+    // store result in result.json
+    writeFile('result.json', JSON.stringify(result, null, 4), (err) => {
+      if (err) {
+        console.log('error writing to file', err);
+      } else {
+        console.log('\nFull result stored in "result.json"');
+      }
+    });
 
     consoleInterface.close();
   });
