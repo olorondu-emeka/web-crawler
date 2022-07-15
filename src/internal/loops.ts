8b56253090a6b20ae069c19b10d7721887cfc9e3
olorondu-emeka <oloronduchukwuemeka@gmail.com>
2022-07-15 03:24:14 +0100

    feat: refactor concurrent looping

--- src/internal/loops.ts
+++ src/internal/loops.ts
@@ -42,3 +42,19 @@ export async function asyncSeriesLoop<T>(
   finalResult.push(...lastResolvedPromise);
   return finalResult;
 }
+
+/**
+ * processes an array of batches concurrently by running each batch in parallel
+ * @param batchArray an array of batches
+ * @param callback callback function for every item in each batch
+ */
+export async function concurrentLoop<T>(
+  batchArray: string[][],
+  callback: (item: string) => Promise<T>
+) {
+  return asyncSeriesLoop<T>(batchArray, async (batch: string[]) => {
+    const batchResult = await asyncParallelLoop<Node>(batch, callback);
+
+    return batchResult;
+  });
+}
